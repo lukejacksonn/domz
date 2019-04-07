f19f412d87c5e1b1ef12607df27d4d05ef06a801
Dustin Dowell <dustindowell22@gmail.com>
2019-04-07 06:21:13 -0500

    use a map

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const cache = {};
+const cache = new Map();
 export default h =>
   new Proxy(
     {},
     {
-      get: (target, name) => cache[name] || (cache[name] = (...args) =>
+      get: (target, name) => cache.get(name) || cache.set(name, (...args) =>
         args[0] && typeof args[0] === "object" && !Array.isArray(args[0])
           ? h(name, ...args)
-          : h(name, {}, ...args))
+          : h(name, {}, ...args)).get(name)
     }
   );
