704c99c0c171ba48679215af149e22d35a63424a
Daniel Loomer <fuzetsu@users.noreply.github.com>
2019-04-09 11:47:42 -0400

    properly handle optional props

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const cache = {};
-export default h =>
-  new Proxy(
+export default h => {
+  const vnodeProps = Object.keys(h("div", {}, "test"));
+  const isVnode = obj => {
+    const keys = Object.keys(obj);
+    return vnodeProps.every(key => keys.includes(key));
+  };
+  return new Proxy(
     {},
     {
       get: (target, name) => cache[name] || (cache[name] = (...args) =>
-        args[0] && typeof args[0] === "object" && !Array.isArray(args[0])
+        typeof args[0] === "object" && !Array.isArray(args[0]) && !isVnode(args[0])
           ? h(name, ...args)
           : h(name, {}, ...args))
     }
   );
+};
